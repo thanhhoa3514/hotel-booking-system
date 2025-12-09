6028fa6aaecbd4e91196aa04d644cd0aa6ac18ee
thanhhoa3514 <ngothanhhoa3514@gmail.com>
2025-12-09 20:42:02 +0700

    refactor(server): add dockercompose and seting up the helmet package

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from 'nestjs-pino';
+import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   app.useLogger(app.get(Logger));
-  await app.listen(process.env.PORT ?? 3000);
+  app.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: ["'self'"],
+          scriptSrc: ["'self'", "'unsafe-inline'"],
+          styleSrc: ["'self'", "'unsafe-inline'"],
+          imgSrc: ["'self'", 'data:', 'https:'],
+        },
+      },
+      // Nếu bạn load ảnh từ domain khác (ví dụ S3), cần mở cái này
+      crossOriginResourcePolicy: { policy: 'cross-origin' },
+    }),
+  );
+  app.enableCors({
+    origin: 'http://localhost:3000', // Chỉ cho phép Next.js gọi
+    credentials: true, // Cho phép gửi cookie/token
+  });
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
